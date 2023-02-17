6e09234c6a555bd819d17a87ac7b044a61e1d5cb
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2023-02-17 09:44:15 +0200

    finished step5

---
+++ Basic01/Step_5.php
@@ -0,0 +1,11 @@
+<?php
+$course_name = "Rust, the language of no bugs";
+$enrolled_students = 20;
+$price = 30.00;
+$on_discount = true;
+echo "Course title: $course_name<br>";
+echo "Enrolled Students: $enrolled_students<br>";
+echo "Course price: $price $ USD<br>";
+echo "Course on discount: " . ($on_discount ? "Yes" : "No") . "<br>";
+echo "Course not on discount: " . (!$on_discount ? "Yes" : "No") . "<br>";
+?>
