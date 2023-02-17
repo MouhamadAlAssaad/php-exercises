f691e302f51213cda9ec292d9f7b9c2ee8502a15
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2023-02-17 09:19:01 +0200

    finished step4

---
+++ Step_4.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Mouhamad";
+$position = "Web Developer";
+$github_url = "https://github.com/MouhamadAlAssaad";
+echo "Hello, I'm $name, I'm a $position. Please check my github link $github_url.<br>";
+echo "Hello, I'm {$name}, I'm a {$position}. Please check my github link {$github_url}.<br>";
+echo 'Hello, I\'m ' . $name . ', I\'m a ' . $position . '. Please check my github link ' . $github_url . '.<br>';
+?>
