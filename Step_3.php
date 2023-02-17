62647d508adb4de66441075a204ecdf1cc11a7a2
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2023-02-17 08:37:18 +0200

    finished step3

---
+++ Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Mouhamad";
+$email ="mhamad.said.2182000@gmail.com";
+echo "My name is \"$name\" and my email is \"$email\".";
+$height = 200;
+$width = 100;
+$area = $height * $width;
+echo "The area is $area ."
+?>
