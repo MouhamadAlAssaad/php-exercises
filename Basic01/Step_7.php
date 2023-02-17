074ad4eb0840f432cd4896cd141d4a82c8d462af
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2023-02-17 10:55:37 +0200

    finished step7

---
+++ Basic01/Step_7.php
@@ -0,0 +1,11 @@
+<?php
+
+$birth_year = 2000;
+$current_year = date('Y');
+if ($current_year - $birth_year > 18){
+    echo "You can drive";
+} else {
+    echo "You are still a kid, go and play GTA";
+}
+
+?>
